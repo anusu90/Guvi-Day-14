8d33154bcacbeda8fa0f8def9804ec8c8cde58b5
anunay <anusu90@gmail.com>
2020-11-27 15:16:19 +0530

    formating

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,22 @@
-let select = document.getElementById('food');
+let ss = document.getElementById('food');
 
 
 // WE SHALL SET UP CUSTOM VALIDATION FOR FOOD SELECTION
 // TO ENSURE AT LEAST 2 OPTIONS ARE SLECTED
 
-select.addEventListener('click', ()=> {
+ss.addEventListener('click', ()=> {
 
-    console.log(select.selectedOptions.length);
-    if (select.selectedOptions.length < 2) {
+    console.log(ss.selectedOptions.length);
+    if( ss.selectedIndex === -1){
 
-        select.setCustomValidity('Please Select at least 2 options'); //SET UP CUSTOM MESSAGE
+        ss.setCustomValidity('Please Select at least 2 options'); //SET UP CUSTOM MESSAGE
+        console.log('here')
+
+    } else if (ss.selectedOptions.length < 2) {
+
+        ss.setCustomValidity('Please Select at least 2 options'); //SET UP CUSTOM MESSAGE
     } else {
-        select.setCustomValidity('');
+        ss.setCustomValidity('');
     }
 });
 
@@ -22,10 +27,6 @@ form = document.getElementById('my-form');
 form.addEventListener('submit', (e)=>{
 
     e.preventDefault(); // PREVENT DEFAULT
-
-
-
-
     formData = new FormData(form);
 
     // USE ARRAY DESTRUCTUING TO LOOP THROUGH THE ELEMENTS OF FORMDATA AND APPEND THEM TO TBODY
